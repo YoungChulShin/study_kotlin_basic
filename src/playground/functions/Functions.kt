58899b176cb1eea12dd46f06f78c65589ca78169
YoungChulShin <go1323@gmail.com>
2021-11-15 22:50:59 +0900

    infix function

--- src/playground/functions/Functions.kt
+++ src/playground/functions/Functions.kt
@@ -1,4 +1,4 @@
-package playground
+package playground.functions
 
 fun main() {
     /**
---
+++ src/playground/functions/InfixFunctions.kt
@@ -0,0 +1,26 @@
+package playground.functions.InfixFunctions
+
+fun main() {
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "bye")
+
+    val pair = "Ferrari" to "Katrina"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val cloudia = Person("Claudia")
+    sophia likes cloudia
+
+    println(sophia.linkedPeople[0].name)
+}
+
+class Person(val name: String) {
+    val linkedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {
+        linkedPeople.add(other)
+    }
+}
