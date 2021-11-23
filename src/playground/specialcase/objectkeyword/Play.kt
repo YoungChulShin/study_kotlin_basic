62b064f27ad01a012cdb3c627364f98f5bab4be6
YoungChulShin <go1323@gmail.com>
2021-11-23 23:11:19 +0900

    object keyword

---
+++ src/playground/specialcase/objectkeyword/Play.kt
@@ -0,0 +1,58 @@
+package playground.specialcase.objectkeyword
+
+import java.util.*
+
+class LuckDispatcher {
+    fun getNumber() {
+        val objRandom = Random()
+        println(objRandom.nextInt(90))
+    }
+}
+
+fun rentPrice(standardDays: Int, festivityDays: Int, specialDays: Int): Unit {
+
+    val dayRates = object {
+        var standard: Int = 30 * standardDays
+        var festivity: Int = 50 * festivityDays
+        var special: Int = 100 * specialDays
+
+        fun printData() = println("print data")
+    }
+
+    val total = dayRates.standard + dayRates.festivity + dayRates.special
+    print("Total price: $$total")
+}
+
+object DoAuth {
+    fun takeParams(username: String, password: String) {        //2
+        println("input Auth parameters = $username:$password")
+    }
+}
+
+class BigBen {
+    companion object Bonger {
+        fun getBongs(nTimes: Int) {
+            for (i in 1 .. nTimes) {
+                print("BONG ")
+            }
+        }
+    }
+}
+
+fun main() {
+    val d1 = LuckDispatcher()
+    val d2 = LuckDispatcher()
+
+    d1.getNumber()
+    d2.getNumber()
+
+    rentPrice(10, 2, 1)
+
+    DoAuth.takeParams("foo", "qwerty")
+
+    val binBen = BigBen()
+
+    BigBen.Bonger.getBongs(3)
+    BigBen.getBongs(3)
+}
+
